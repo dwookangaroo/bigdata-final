d57312dbc698d16554de1640f3bf36da513816dd
hjs429 <hjs928@naver.com>
2021-10-20 18:18:43 +0900

    delete bad server file

--- ImgProcessing/ImgAnal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from ImgAnal.models import MyImage, Addresses
-
-admin.site.register(MyImage)
-admin.site.register(Addresses)
--- ImgProcessing/ImgAnal/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImganalConfig(AppConfig):
-    default_auto_field = 'django.db.models.AutoField'
-    name = 'ImgAnal'
